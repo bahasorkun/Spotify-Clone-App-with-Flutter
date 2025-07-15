c5a5e214ceed63df6f96bbf16f06af5f4816d739
Baha Sorkun <114817265+bahasorkun@users.noreply.github.com>
2025-07-15 15:42:50 +0300

    Update register screen spacing and add Apple/Google icons
    - Register sayfasında inputlar arası boşlukları düzenledim
    - Apple ve Google oturum açma ikonlarını assetlere ekledim
    - basic_app_button import edildi

--- spotify_clone_app/lib/presentation/auth/pages/register.dart
+++ spotify_clone_app/lib/presentation/auth/pages/register.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:spotify_clone_app/common/helpers/is_dark_mode.dart';
 import 'package:spotify_clone_app/common/widgets/appbar/app_bar.dart';
+import 'package:spotify_clone_app/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_clone_app/core/configs/assets/app_vectors.dart';
 import 'package:spotify_clone_app/core/configs/theme/app_colors.dart';
 import 'package:spotify_clone_app/presentation/auth/pages/support_page.dart';
@@ -57,17 +58,43 @@ class Register extends StatelessWidget {
             ),
           ),
           const SizedBox(
-            height: 25,
+            height: 20,
           ),
           _fullNameField(context),
           const SizedBox(
-            height: 15,
+            height: 20,
           ),
           _enterEmailField(context),
           const SizedBox(
-            height: 15,
+            height: 20,
           ),
           _passwordField(context),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 29.0),
+            child: BasicAppButton(onPressed: () {}, title: "Create Account"),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          //Yatay çizgi ve "Or" metni
+          Row(
+            children: [
+              Expanded(child: Divider(thickness: 1)),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text("Or"),
+              ),
+              Expanded(child: Divider(thickness: 1)),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          //Google ve Apple butonları
+          Row()
         ],
       ),
     );
@@ -86,26 +113,35 @@ class Register extends StatelessWidget {
   }
 
   Widget _fullNameField(BuildContext context) {
-    return TextField(
-      decoration: const InputDecoration(
-        hintText: "Full Name",
-      ).applyDefaults(Theme.of(context).inputDecorationTheme),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28.0),
+      child: TextField(
+        decoration: const InputDecoration(
+          hintText: "Full Name",
+        ).applyDefaults(Theme.of(context).inputDecorationTheme),
+      ),
     );
   }
 
   Widget _enterEmailField(BuildContext context) {
-    return TextField(
-      decoration: const InputDecoration(
-        hintText: "Enter E-mail",
-      ).applyDefaults(Theme.of(context).inputDecorationTheme),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28.0),
+      child: TextField(
+        decoration: const InputDecoration(
+          hintText: "Enter E-mail",
+        ).applyDefaults(Theme.of(context).inputDecorationTheme),
+      ),
     );
   }
 
   Widget _passwordField(BuildContext context) {
-    return TextField(
-      decoration: const InputDecoration(
-        hintText: "Password",
-      ).applyDefaults(Theme.of(context).inputDecorationTheme),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28.0),
+      child: TextField(
+        decoration: const InputDecoration(
+          hintText: "Password",
+        ).applyDefaults(Theme.of(context).inputDecorationTheme),
+      ),
     );
   }
 }
