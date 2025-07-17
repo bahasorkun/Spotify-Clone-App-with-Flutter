6c31d6dfa9521b78d9a85fa529bdb2024f609b06
Baha Sorkun <114817265+bahasorkun@users.noreply.github.com>
2025-07-17 12:30:28 +0300

    Initalized firebase

--- spotify_clone_app/lib/main.dart
+++ spotify_clone_app/lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify_clone_app/core/configs/theme/app_theme.dart';
+import 'package:spotify_clone_app/firebase_options.dart';
 import 'package:spotify_clone_app/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:spotify_clone_app/presentation/splash/pages/splash.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorageDirectory.web
